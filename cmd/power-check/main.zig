72b70271775337d068c5b663a8ed5d81292185c4
Tim Brockley <timbrockley@gmail.com>
2025-04-13 22:20:52 +0100

    updated power-check command

--- cmd/power-check/main.zig
+++ cmd/power-check/main.zig
@@ -2,7 +2,42 @@
 // Copyright 2025, Tim Brockley. All rights reserved.
 // This software is licensed under the MIT License.
 //--------------------------------------------------------------------------------
+
 const std = @import("std");
+
+const MAX_OUTPUT_BYTES: usize = 2 * 1024;
+
+//--------------------------------------------------------------------------------
+
+pub fn child_process(
+    allocator: std.mem.Allocator,
+    argv: []const []const u8,
+    stdout: *std.ArrayListUnmanaged(u8),
+    stderr: *std.ArrayListUnmanaged(u8),
+    maxOutputBytes: usize,
+) !u8 {
+    //------------------------------------------------------------
+    var child = std.process.Child.init(
+        argv,
+        allocator,
+    );
+    child.stdout_behavior = .Pipe;
+    child.stderr_behavior = .Pipe;
+    //------------------------------------------------------------
+    try child.spawn();
+    try child.collectOutput(
+        allocator,
+        stdout,
+        stderr,
+        maxOutputBytes,
+    );
+    //------------------------------------------------------------
+    const term = try child.wait();
+    return term.Exited;
+    //------------------------------------------------------------
+}
+
+//--------------------------------------------------------------------------------
 
 pub fn main() !void {
     //------------------------------------------------------------
@@ -10,29 +45,29 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     //------------------------------------------------------------
-    var child = std.process.Child.init(&[_][]const u8{ "upower", "--dump" }, allocator);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Pipe;
-    //------------------------------------------------------------
     var stdout = std.ArrayListUnmanaged(u8){};
-    defer stdout.deinit(allocator);
     var stderr = std.ArrayListUnmanaged(u8){};
+    defer stdout.deinit(allocator);
     defer stderr.deinit(allocator);
     //------------------------------------------------------------
-    try child.spawn();
-    try child.collectOutput(allocator, &stdout, &stderr, 10240);
-    const term = child.wait() catch |err| {
+    const exitCode = child_process(
+        allocator,
+        &[_][]const u8{ "upower", "--dump" },
+        &stdout,
+        &stderr,
+        MAX_OUTPUT_BYTES,
+    ) catch |err| {
         std.debug.print("process error: {s}\n", .{@errorName(err)});
         std.process.exit(1);
     };
     //------------------------------------------------------------
-    if (stderr.items.len == 0) {
+    if (stderr.items.len > 0) {
         try std.io.getStdErr().writer().writeAll(stderr.items);
     }
     //------------------------------------------------------------
-    if (term.Exited > 0) {
-        std.debug.print("invalid exit code: {d}\n", .{term.Exited});
-        std.process.exit(term.Exited);
+    if (exitCode > 0) {
+        std.debug.print("invalid exit code: {d}\n", .{exitCode});
+        std.process.exit(exitCode);
     }
     //------------------------------------------------------------
     var usingBattery = false;
@@ -76,3 +111,5 @@ pub fn main() !void {
     std.process.exit(0);
     //------------------------------------------------------------
 }
+
+//--------------------------------------------------------------------------------
