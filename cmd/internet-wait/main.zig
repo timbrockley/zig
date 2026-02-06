68af71543eb9dd6197a56389b3a60d729edb1707
Tim Brockley <timbrockley@gmail.com>
2026-02-06 17:46:13 +0000

    updated internet-wait

--- cmd/internet-wait/main.zig
+++ cmd/internet-wait/main.zig
@@ -11,7 +11,7 @@ const stdout = &stdout_writer.interface;
 const ADDR: []const u8 = "1.1.1.1";
 const PORT: u16 = 53;
 
-const MAX_LEN: u8 = 10;
+const MAX_BAR_LEN: u8 = 10;
 const DURATION: u64 = 100 * std.time.ns_per_ms;
 
 //--------------------------------------------------------------------------------
@@ -23,14 +23,14 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     var bar_index: u8 = 0;
-    var bar: [MAX_LEN]u8 = undefined;
+    var bar: [MAX_BAR_LEN]u8 = undefined;
 
     while (!connected(allocator)) {
         fillBar(&bar, bar_index);
 
         try stdout.print("\rWaiting for internet connection [{s}]", .{bar});
 
-        bar_index = if (bar_index < MAX_LEN) bar_index + 1 else 1;
+        bar_index = if (bar_index < MAX_BAR_LEN) bar_index + 1 else 1;
 
         std.Thread.sleep(DURATION);
     }
@@ -38,8 +38,10 @@ pub fn main() !void {
     try stdout.writeAll("\r\x1b[2K"); // carriage return + clear line
 }
 
-fn fillBar(bar: *[MAX_LEN]u8, bar_index: u8) void {
-    const index: u8 = if (bar_index < MAX_LEN) bar_index else MAX_LEN;
+//--------------------------------------------------------------------------------
+
+fn fillBar(bar: *[MAX_BAR_LEN]u8, bar_index: u8) void {
+    const index: u8 = if (bar_index < MAX_BAR_LEN) bar_index else MAX_BAR_LEN;
     for (bar[0..index]) |*b| {
         b.* = '.';
     }
