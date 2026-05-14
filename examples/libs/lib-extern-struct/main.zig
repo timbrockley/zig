2a2ea19bc76da6da48282932a76740db80a88794
Tim Brockley <timbrockley@gmail.com>
2026-05-14 14:45:42 +0100

    updted libs code examples

--- examples/libs/lib-extern-struct/main.zig
+++ examples/libs/lib-extern-struct/main.zig
@@ -8,9 +8,9 @@ pub const ExampleEnum = enum(u8) { no = 0, yes = 1 };
 pub const ExampleRawString = extern struct { ptr: [*]const u8, len: usize };
 //--------------------------------------------------------------------------------
 pub const Self = extern struct {
-    example_byte: u8,
-    example_enum: ExampleEnum,
-    example_raw_string: ExampleRawString,
+    example_byte: u8 = 0,
+    example_enum: ExampleEnum = .no,
+    example_raw_string: ExampleRawString = .{ .ptr = "", .len = 0 },
 };
 //--------------------------------------------------------------------------------
 // extern fn init() Self;
@@ -41,16 +41,7 @@ pub fn main() !void {
     //--------------------------------------------------------------------------------
     // var self = init();
     //------------------------------------------------------------
-    const example_string = "";
-    //------------------------------------------------------------
-    var self = Self{
-        .example_byte = 0,
-        .example_enum = .no,
-        .example_raw_string = ExampleRawString{
-            .ptr = example_string.ptr,
-            .len = example_string.len,
-        },
-    };
+    var self = Self{};
     //--------------------------------------------------------------------------------
     {
         //----------------------------------------
